46506bd905fe717e94305a3fcf8a2cd38a546838
Suji <soojiya2003@gmail.com>
2025-11-24 20:47:21 +0900

    refactor: Home 데이터 가공 로직 수정
    - Home 컴포넌트에서 bookmarkBar data만 정제해서 prop으로 넘기는 로직으로 수정

--- mission-ssujissuji/src/pages/Home.tsx
+++ mission-ssujissuji/src/pages/Home.tsx
@@ -1,60 +1,32 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import BookMarkCardList from '../components/home/BookMarkCardList';
 import { useBookmarks } from '../hooks/useBookmark';
-import type { OutletContextType } from './DetailPage';
-import { useOutletContext } from 'react-router';
-import { sortFolders } from '../utils/sortFolders';
-import { separateFolderAndBookmarks } from '../utils/bookmarkTreeUtils';
 
 export default function Home() {
   const { data } = useBookmarks();
-  const { sortType } = useOutletContext<OutletContextType>();
+  const [bookmarkBarData, setBookmarkBarData] = useState<BookmarkTreeType[]>(
+    [],
+  );
 
-  const [bookmarkBar, setBookmarkBar] = useState<BookmarkTreeType>([]);
-
-  const { folders } = separateFolderAndBookmarks(bookmarkBar);
-
-  // bookmarkbar 와 기타 북마크 구분
-  const bookmarkBarSeparate = (data: BookmarkTreeType) => {
+  useEffect(() => {
     if (!data || !data[0]?.children) {
       console.error('Invalid data structure:', data);
       return;
     }
 
     const rootLevel = data[0].children;
-
     const rootBar = rootLevel.find(
       (node: BookmarkTreeType) => node.title === '북마크바',
     );
-    const rootOther = rootLevel.find(
-      (node: BookmarkTreeType) => node.title === '기타 북마크',
-    );
-
-    console.log('rootBar', rootBar);
-    console.log('rootOther', rootOther);
 
     if (rootBar?.children) {
-      setBookmarkBar(rootBar.children);
+      setBookmarkBarData(rootBar.children); // 전체 내용을 저장
     }
-  };
-
-  // 1. data → bookmarkBar / bookmarkOther 분리
-  useEffect(() => {
-    if (!data || !Array.isArray(data)) {
-      console.error('Data is not valid:', data);
-      return;
-    }
-    bookmarkBarSeparate(data);
   }, [data]);
-
-  const sortedFolders = useMemo(
-    () => sortFolders(folders, sortType),
-    [folders, sortType],
-  );
 
   return (
     <div>
-      <BookMarkCardList bookmarkBar={sortedFolders} />
+      <BookMarkCardList bookmarkBar={bookmarkBarData} />
     </div>
   );
 }
