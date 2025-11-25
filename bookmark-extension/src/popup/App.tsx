7abcfbc09c2d12ad50cc36cda29db7e3f0c205db
Suji <soojiya2003@gmail.com>
2025-11-25 23:46:52 +0900

    refactor(App): 크롬 확장프로그램 등록 리젝 부분 개선 - localhost 코드 삭제 - loading 상태 변화 변경 추가

--- bookmark-extension/src/popup/App.tsx
+++ bookmark-extension/src/popup/App.tsx
@@ -5,32 +5,33 @@ import { useState } from 'react';
 function App() {
   const [loading, setLoading] = useState(false);
 
-  const handleExport = async () => {
+  const handleExport = () => {
     setLoading(true);
     chrome.bookmarks.getTree((bookmarks) => {
       console.log('📂 내 북마크 데이터:', bookmarks);
+      setLoading(false);
     });
     // 1️⃣ 크롬 북마크 트리 가져오기
-    chrome.bookmarks.getTree(async (tree) => {
-      try {
-        // 2️⃣ Express 서버로 데이터 전송
-        const res = await fetch('http://localhost:3001/api/bookmarks', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(tree),
-        });
+    // chrome.bookmarks.getTree(async (tree) => {
+    //   try {
+    //     // 2️⃣ Express 서버로 데이터 전송
+    //     const res = await fetch('http://localhost:3001/api/bookmarks', {
+    //       method: 'POST',
+    //       headers: { 'Content-Type': 'application/json' },
+    //       body: JSON.stringify(tree),
+    //     });
 
-        if (res.ok) {
-          console.log('✅ 북마크 전송 성공!');
-        } else {
-          console.error('❌ 전송 실패');
-        }
-      } catch (error) {
-        console.error('⚠️ 오류:', error);
-      } finally {
-        setLoading(false);
-      }
-    });
+    //     if (res.ok) {
+    //       console.log('✅ 북마크 전송 성공!');
+    //     } else {
+    //       console.error('❌ 전송 실패');
+    //     }
+    //   } catch (error) {
+    //     console.error('⚠️ 오류:', error);
+    //   } finally {
+    //     setLoading(false);
+    //   }
+    // });
   };
 
   return (
