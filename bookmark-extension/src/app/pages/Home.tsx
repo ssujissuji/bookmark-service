355da7e9eaf3fb53e9003f28d415e330d62dc40f
Suji <soojiya2003@gmail.com>
2025-12-01 10:33:22 +0900

    chore: 불필요한 console.log 제거

--- bookmark-extension/src/app/pages/Home.tsx
+++ bookmark-extension/src/app/pages/Home.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   const [bookmarkBarData, setBookmarkBarData] = useState<BookmarkTreeType[]>(
     [],
   );
-  console.log('Home data:', data);
+
   useEffect(() => {
     if (!data || !data[0]?.children) {
       console.error('Invalid data structure:', data);
