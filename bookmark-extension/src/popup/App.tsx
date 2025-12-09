cc61a10254650a437c2ed86587ab89eba3e26912
Suji <soojiya2003@gmail.com>
2025-12-09 22:38:38 +0900

    refactor: App 컴포넌트 정리 및 북마크 업데이트 로직 수정

--- bookmark-extension/src/popup/App.tsx
+++ bookmark-extension/src/popup/App.tsx
@@ -1,59 +1,39 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import AppView from '../app/App';
 import Modal from 'react-modal';
+import { Toaster } from 'react-hot-toast';
 
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-    borderRadius: '12px',
-  },
-};
+// const customStyles = {
+//   content: {
+//     top: '50%',
+//     left: '50%',
+//     right: 'auto',
+//     bottom: 'auto',
+//     marginRight: '-50%',
+//     transform: 'translate(-50%, -50%)',
+//     borderRadius: '12px',
+//   },
+// };
 
 Modal.setAppElement('#root'); // 모달 접근성 설정
 
 function App() {
-  const [modalIsOpen, setIsOpen] = useState(true);
-  const [bookmarksData, setBookmarksData] = useState<BookmarkTreeType[] | null>(
-    null,
-  );
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState<Error | null>(null);
+  //   const [modalIsOpen, setIsOpen] = useState(true);
 
-  const handleExport = () => {
-    setLoading(true);
-    chrome.bookmarks.getTree((bookmarks) => {
-      if (!bookmarks) {
-        setError(new Error('Failed to retrieve bookmarks'));
-        setLoading(false);
-        return;
-      }
-      console.log('📂 내 북마크 데이터:', bookmarks);
-      setBookmarksData(bookmarks as BookmarkTreeType[]);
-      setLoading(false);
-    });
-    setIsOpen(false);
-  };
+  //   const handleExport = () => {
+  //     setIsOpen(false);
+  //   };
 
-  const closeModal = () => {
-    setIsOpen(false);
-  };
-  const afterCloseModal = () => {
-    // Additional actions after modal opens can be added here
-  };
-
-  if (error) {
-    console.log('Error', error.message);
-    return <div>Error: {error.message}</div>;
-  }
+  //   const closeModal = () => {
+  //     setIsOpen(false);
+  //   };
+  //   const afterCloseModal = () => {
+  //     // Additional actions after modal opens can be added here
+  //   };
 
   return (
     <>
-      <Modal
+      {/* <Modal
         isOpen={modalIsOpen}
         onRequestClose={closeModal}
         onAfterOpen={afterCloseModal}
@@ -70,13 +50,15 @@ function App() {
           <button
             className="rounded-2xl border border-(--color-main-red) px-4 py-2 mt-4 text-black hover:text-white hover:bg-(--color-main-red) cursor-pointer"
             onClick={handleExport}
-            disabled={loading}
           >
-            {loading ? '전송 중...' : '내 웹 서비스로 보내기'}
+            내 북마크 불러오기
           </button>
         </div>
-      </Modal>
-      <AppView data={bookmarksData} loading={loading} error={error} />
+      </Modal> */}
+      <div>
+        <Toaster />
+        <AppView />
+      </div>
     </>
   );
 }
