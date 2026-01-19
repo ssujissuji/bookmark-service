edc9c5fef7831b849bda559b720f23fc25c4c4a1
Suji <soojiya2003@gmail.com>
2026-01-20 00:26:02 +0900

    refactor(TextButton): 중첩 css 제거

--- bookmark-extension/src/app/components/ui/TextButton.tsx
+++ bookmark-extension/src/app/components/ui/TextButton.tsx
@@ -18,7 +18,7 @@ export default function TextButton({
   return (
     <button className={textStyle} onClick={onClick}>
       {children}
-      <span>{buttonName}</span>
+      {buttonName}
     </button>
   );
 }
