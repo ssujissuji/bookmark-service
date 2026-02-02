3845c94dca94990f043acf7b63ddfabaf1442d4d
Suji <soojiya2003@gmail.com>
2026-02-02 23:26:55 +0900

    refactor(ui) : 검색 결과가 없을 경우 해당 문구의 ui 위치 자연스럽게 보이도록 변경 -(변경 전) 폴더 목록 가운데에 부자연스러운 ui 로 보여졌었음 -(변경 후) 폴더 목록 마지막에 한줄로 들어가 자연스러운 ui로 개선

--- bookmark-extension/src/app/components/home/BookMarkCardList.tsx
+++ bookmark-extension/src/app/components/home/BookMarkCardList.tsx
@@ -103,23 +103,24 @@ export default function BookMarkCardList({
           type="others"
           onClick={() => navigate('/bookmark/2')}
         />
-        {hasSearch && sortedFolderList.length === 0 ? (
-          <p className="mt-4 text-sm text-gray-400">검색 결과가 없습니다.</p>
-        ) : (
-          sortedFolderList.map((list) => (
-            <BookmarkCard
-              key={list.id}
-              title={list.title}
-              id={list.id}
-              dateAdded={list.dateAdded}
-              type="bookmarkBar"
-              onClick={() => navigate(`/bookmark/${list.id}`)}
-            />
-          ))
-        )}
+        {hasSearch && sortedFolderList.length === 0
+          ? ''
+          : sortedFolderList.map((list) => (
+              <BookmarkCard
+                key={list.id}
+                title={list.title}
+                id={list.id}
+                dateAdded={list.dateAdded}
+                type="bookmarkBar"
+                onClick={() => navigate(`/bookmark/${list.id}`)}
+              />
+            ))}
 
         <NewBookMark />
       </ul>
+      {hasSearch && sortedFolderList.length === 0 && (
+        <p className="mt-4 text-sm text-gray-400">검색 결과가 없습니다.</p>
+      )}
 
       <div className="flex flex-col justify-start items-start w-full gap-4">
         <div className="w-full flex justify-between items-center">
