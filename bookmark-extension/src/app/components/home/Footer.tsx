39bc61e964af8a7eaccc2bbc1472fff9a2727162
Suji <soojiya2003@gmail.com>
2026-02-08 18:15:40 +0900

    chore (Footer): ver 1.5.3 수정

--- bookmark-extension/src/app/components/home/Footer.tsx
+++ bookmark-extension/src/app/components/home/Footer.tsx
@@ -63,7 +63,7 @@ export default function Footer({
         <div className="mx-auto flex h-12 items-center justify-between px-4 text-[12px] text-white/80">
           {/* LEFT */}
           <div className="flex min-w-0 items-center gap-2">
-            <span className="truncate">ver 1.5.2</span>
+            <span className="truncate">ver 1.5.3</span>
             <span className="truncate">
               © {new Date().getFullYear()} {teamName}
             </span>
